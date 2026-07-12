b3e8b3ce07c016af4ba307f35df58b8980e3982f
Tejasree <tejasreep2024@gmail.com>
2026-07-13 00:26:32 +0530

    Add files via upload

---
+++ Anagram/Demo1.java
@@ -0,0 +1,18 @@
+// Anagram : Anagram means two strings contain the same characters with the same frequency, but the order can be different.
+
+class  Demo1{
+    public static void main(String[] args) {
+        String s1 = "Silent";
+        String s2 = "Listener";
+
+        String S1=s1.toLowerCase();
+        String S2=s2.toLowerCase();
+
+        char ch1[]=s1.toCharArray();
+        char ch2[]=s2.toCharArray();
+         
+        
+
+        
+    }
+}
