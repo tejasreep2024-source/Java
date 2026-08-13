0d7837ca4ed8b0b378ce43cab735d79ccdd7c304
Tejasree <tejasreep2024@gmail.com>
2026-08-13 23:16:51 +0530

    Add files via upload

---
+++ collections/introduction/Demo13.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Demo13{
+    public static void main(String args[]){
+        // List l = new ArrayList();
+        // l.add(0,1);
+        // l.add(1,2);
+        // l.add(2,null);
+        // l.add(3,3);
+        // l.add(4,null);
+        // System.out.println(l);
+
+        Set s = new HashSet();
+        s.add("sandhya");
+        s.add(null);
+        s.add("somya");
+        s.add("shivani");
+        s.add(null);
+        s.add("teja");
+        System.out.println(s);
+
+
+    }
+}
